8f1b98dad7be017abeeb4349e3c535042394bdd7
fares anes <58134689+cptfares@users.noreply.github.com>
2025-02-11 14:35:27 +0100

    Update purchasesRoute.js

--- Routes/purchasesRoute.js
+++ Routes/purchasesRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const purchaseController = require('../controllers/purchaseController ');
+const purchaseController = require('../controllers/purchaseController');
 
 const router = express.Router();
 
